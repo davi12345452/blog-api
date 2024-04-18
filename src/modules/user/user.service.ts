5b10f1e76279579a179ec7d40834eb866d15e848
DaviMaia <davijanischmaia@gmail.com>
2024-04-18 10:22:19 -0300

    feat: comecando a mexer no user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { BadRequestSwagger } from 'src/swagger/helpers/BadRequestError';
 
+/***
+ * LISTA DE ERROS NO USER SERVICE (USER_ERROR-{}):
+ * - 01: Email que se tenta cadastrar já está em uso
+ */
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+    if (user) {
+      const messageError = new BadRequestSwagger(
+        'Email already in user',
+        'The email passed to create account has already in use',
+        'USER_ERROR-01',
+        'POST {api_domain}/user/',
+      );
+      throw new BadRequestException(messageError);
+    }
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action ${updateUserDto} updates a #${id} user`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} user`;
   }
 }
